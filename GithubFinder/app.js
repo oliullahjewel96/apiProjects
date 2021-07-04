b88fb33e5cd6841854b68044a265ab30b3d313c4
oli ullah jewel <oli.jewel@northsouth.edu>
2021-07-05 00:28:19 +0600

    Added method to show repositories.

--- GithubFinder/app.js
+++ GithubFinder/app.js
@@ -25,6 +25,7 @@ searchUser.addEventListener('keyup', (e)=>{
             } else {
                 //Show Profile
                 ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
             }
         })
     }else{
--- GithubFinder/github.js
+++ GithubFinder/github.js
@@ -2,16 +2,24 @@ class Github{
     constructor(){
         this.client_id = 'a10a7aac2ae471aa1f05';
         this.client_secret = 'ab8a1312222eb088d5b865fab1a2e3bf526d75cb';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
     async getUser(user){
         
         const profileResponse = await fetch(`https://api.github.com/users/${user}
         ?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?
+        per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=
+        ${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return{
-            profile
+            profile,
+            repos
         }
 
 
